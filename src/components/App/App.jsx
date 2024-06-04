66046896ee251264e4d865017564dd1885614aff
lisaatea <lisaatea@gmail.com>
2024-06-04 13:41:14 -0700

    initial update all components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,123 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import { coordinates, APIkey } from "../../utils/constants";
+import { getWeather, filterWeatherData } from "../../utils/weatherApi";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/ItemModal";
+import Footer from "../Footer/Footer";
+
+function App() {
+  const [weatherData, setWeatherData] = useState({
+    type: "",
+    temp: { F: 999 },
+    city: "",
+  });
+
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+
+  const handleCardClick = (card) => {
+    setActiveModal("preview");
+    setSelectedCard(card);
+  };
+
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
+  useEffect(() => {
+    getWeather(coordinates, APIkey)
+      .then((data) => {
+        const filteredData = filterWeatherData(data);
+        setWeatherData(filteredData);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+        <Footer />
+      </div>
+      <ModalWithForm
+        title="New garment"
+        buttonText="Add garment"
+        activeModal={activeModal}
+        onClose={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name {""}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image {""}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-btns">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input
+              type="radio"
+              className="modal__radio-input"
+              name="weather"
+              id="hot"
+              value="hot"
+            />
+            <span className="modal__text-radio">Hot</span>
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              type="radio"
+              className="modal__radio-input"
+              name="weather"
+              id="warm"
+              value="warm"
+            />
+            <span className="modal__text-radio">Warm</span>
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              type="radio"
+              className="modal__radio-input"
+              name="weather"
+              id="cold"
+              value="cold"
+            />
+            <span className="modal__text-radio">Cold</span>
+          </label>
+        </fieldset>
+      </ModalWithForm>
+      <ItemModal
+        activeModal={activeModal}
+        card={selectedCard}
+        onClose={closeActiveModal}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer__content">
+      <p className="footer__text">Developed by Lisa Drake</p>
+      <p className="footer__text">2024</p>
+    </div>
+  );
+}
+
+export default Footer;
